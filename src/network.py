7b5c3326a8a23dde7213e89d8a3f875ae4515d2b
Joshua Fajardo <jdfajardo@berkeley.edu>
2020-05-21 14:00:12 -0700

    Changed from lists to dicts, finished backpropogation

--- src/network.py
+++ src/network.py
@@ -6,12 +6,13 @@ class Network:
         Layers is a list of the number of neurons in each layer, with layers[0] being the number of
         neurons in the input layer.
         """
-        self.weights = []
-        self.biases = []
-        self.layers = layers
-        for i in range(len(layers) - 1):
-            self.weights[i] = np.random.randn(layers[i + 1], layers[i])
-            self.biases[i] = np.random.randn(layers[i + 1], 1)
+        self.weights = {}
+        self.biases = {}
+        self.layers = layers  # length L
+        self.L = len(layers)
+        for i in range(2, len(layers)):
+            self.weights[i] = np.random.randn(layers[i - 1], layers[i - 2])
+            self.biases[i] = np.random.randn(layers[i - 1], 1)
 
     def feedforward(self, data):
         """
@@ -19,44 +20,39 @@ class Network:
         Returns an array of the activations in the last layer.
         """
         activations = data
-        for i in range(len(self.weights) - 1):
+        for i in range(2, self.L):
             activations = relu(self.weights[i] @ activations + self.biases[i])
         return activations
 
     def feedforward_transparent(self, data):
-        activations = [np.zeros((self.layers[size], 1)) for size in self.layers]
-        activations[0, :] = data
-        z_values = []
-        for i in range(len(self.weights) - 1):
-            z = self.weights[i + 1] @ activations[i] + self.biases[i + 1]
-            z_values.append(z)
-            activations[i, :] = relu(z)
+        activations = {1: data}
+        z_values = {}
+        for i in range(2, self.L):
+            z = self.weights[i] @ activations[i - 1] + self.biases[i]
+            z_values[i] = z
+            activations[i] = relu(z)
         return activations, z_values
 
     def backpropogate(self, data, label):
         activations, z_values = self.feedforward_transparent(data)
-        wg = [np.zeros((self.layers[j + 1], self.layers[j])) for j in range(len(self.layers) - 1)]
-        bg = [np.zeros((self.layers[j], 1)) for j in range(1, len(self.layers))]
-        for j in range(len(wg[-1])):
-            for k in range(len(wg[-2])):
-                partial_wg = activations[-2, k] * relu_derivative(z_values[-1]) * (activations[-1] - label)
-        dC_dActivations = activations[-1] - label
-        for layer in range(len(wg) - 2, -1, -1):
-            weights = self.weights[layer]
-            for j in range(weights.shape[0]):
-                for k in range(weights.shape[1]):
-                    #todo
-                    partial_wg = activations[layer - 1, k] * relu_derivative(z_values[layer]) * dC_dActivations #todo finish this eq
+        errors = {self.L: (activations[-1] - label) * relu_derivative(z_values[-1])}
+        wg = {self.L: errors[self.L] @ activations[self.L - 1].T}
+        bg = {self.L: errors[self.L]}
+        for layer in range(self.L - 1, 1, -1): # L - 1 to 2
+            errors[layer] = ((self.weights[layer + 1]).T @ errors[layer + 1]) * relu_derivative(z_values[layer])
+            wg[layer] = errors[layer] @ activations[layer - 1].T
+            bg[layer] = errors[layer]
+        return wg, bg
 
 
     def train(self, data, labels, batch_size, eta):
         i = 0
         while i < len(data):
             curr_size = min(len(data) - i, batch_size)
-            weight_gradient = [np.zeros((self.layers[j + 1], self.layers[j])) for j in range(len(self.layers) - 1)]
-            bias_gradient = [np.zeros((self.layers[j], 1)) for j in range(1, len(self.layers))]
+            wg = {(j, np.zeros((self.layers[j + 1], self.layers[j]))) for j in range(self.L - 1)}
+            bg = {np.zeros((self.layers[j], 1)) for j in range(1, self.L)}
             for j in range(i, i + curr_size):
-                wg_addend = self.backpropogate(data[j, :], labels[j, :])
+                wg_addend, bg_addend = self.backpropogate(data[j, :], labels[j, :])
 
             i += curr_size
 
